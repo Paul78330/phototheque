3510d46d2085a924e32ded6f5f0d11687feabea7
paul-gaultier <paul.gaultier76@yahoo.com>
2024-02-15 23:15:52 +0100

    update docker-compose.yml

--- index.test.js
+++ index.test.js
@@ -1,31 +1,27 @@
 const mongoose = require('mongoose');
-// Importation du module supertest pour tester les requêtes HTTP
 const request = require('supertest');
-// Importation du module sinon pour créer des espions et des bouchons
 const sinon = require('sinon');
-// Importation du modèle Album pour tester les interactions avec la base de données
 const Album = require('./models/Album');
-// Importation de l'application Express à tester
 const app = require('./index');
 
 let server;
 
-// Avant chaque test, nous démarrons le serveur sur un port aléatoire
 beforeEach((done) => {
   const port = Math.floor(Math.random() * (60000 - 3000 + 1)) + 3000;
   server = app.listen(port, done);
 });
 
-// Après chaque test, nous fermons le serveur et la connexion à MongoDB
-afterEach((done) => {
+afterAll((done) => {
   server.close(() => {
     mongoose.connection.close(done);
   });
 }, 10000);
 
-// Nous définissons un groupe de tests pour la route GET '/'
+afterEach(() => {
+  sinon.restore();
+});
+
 describe('GET /', () => {
-  // Ce test vérifie que la route '/' redirige vers '/albums'
   it('should redirect to /albums', async () => {
     const res = await request(app)
       .get('/')
@@ -34,38 +30,24 @@ describe('GET /', () => {
   });
 });
 
-// Nous définissons un groupe de tests pour la route POST '/album/create'
 describe('POST /album/create', () => {
-  // Ce test vérifie que la route '/album/create' crée un nouvel album
   it('should create a new album', async () => {
     const albumData = { albumTitle: 'Test Album' };
-    // Nous créons un bouchon pour la méthode 'create' de l'Album
     const createStub = sinon.stub(Album, 'create').resolves();
     const res = await request(app)
       .post('/album/create')
       .send(albumData)
       .expect(302);
-    // Nous vérifions que la méthode 'create' a été appelée avec les bonnes données
     sinon.assert.calledWith(createStub, { title: albumData.albumTitle });
-    // Nous vérifions que la réponse redirige vers '/albums'
     expect(res.headers.location).toBe('/albums');
-    // Nous restaurons la méthode 'create' à son comportement original
-    createStub.restore();
   });
 });
 
-// Nous définissons un groupe de tests pour la route GET '/album/:id'
-// ...
-
-// Nous définissons un groupe de tests pour la route GET '/album/:id'
 describe('GET /album/:id', () => {
-  // Ce test vérifie que la route '/album/:id' renvoie le bon statut HTTP
   it('should render the album page', async () => {
-    const idAlbum = '65ce545c9fb0faceda542e88'; // Utilisez l'ID spécifique ici
+    const idAlbum = '65ce545c9fb0faceda542e88';
     const res = await request(app)
       .get(`/album/${idAlbum}`)
-      .expect(302); // Changez le statut attendu en 302 si la redirection est attendue
+      .expect(302);
   });
 });
-
-// ...
